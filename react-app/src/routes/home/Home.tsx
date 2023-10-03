ed7d01831d63331342033007bd6029e7db4def19
MIYAKA <75727824+BambinoRiya@users.noreply.github.com>
2023-10-03 18:10:17 +0000

    finalized design

--- react-app/src/routes/home/Home.tsx
+++ react-app/src/routes/home/Home.tsx
@@ -1,6 +1,6 @@
 import Image1 from "../../assets/levis.png";
 import Image2 from "../../assets/levis-demo.png";
-import Image3 from "../../assets/lady-justice.png"
+import Image3 from "../../assets/lady-justice-no-bg.png"
 import "./Home.css"
 
 
@@ -21,17 +21,20 @@ function Home(){
       </header>
       <div className="content">
         <div className="info">
-        <h2>Your <span className="green-text">legal guide.</span>  Just a <br /> <span className="green-text">chat</span> away</h2>
+        <h2>Your <span className="green-text">legal guide</span>.  Just a <br /> <span className="green-text">chat</span> away!</h2>
         </div>
         </div>
 
       <div className="justice-container">
       <img src={Image3}  alt="Lady Justice" className="lady-justice"/> </div>
 
-      <div className="demo-cointainer"><img src={Image2} className="demo"/></div>
+      <div className="demo-container"><img src={Image2} className="demo"/></div>
         
+      <div className="use-ai-btn-container"> 
+      <p>WANT SOME QUICK <br />LEGAL ADVICE?</p>
+      <a href="#" className="use-ai-btn">TRY FOR FREE</a>
+      </div>
       
-      <a href="#" className="use-ai-btn">Try For Free</a>
     </section>
   )
 }
